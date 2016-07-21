83349ce6278d4d43356abe6b808cf4a76fe68e22
jpg207 <jpg207@gmail.com>
2016-07-21 18:10:09 +1200

    Bunny Run 0.0.1

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+    private Rigidbody2D MyRigidBody;
+    public float BunnyJumpForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+        MyRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonUp("Jump"))
+        {
+            MyRigidBody.AddForce(transform.up * BunnyJumpForce);
+        }
+	}
+}
