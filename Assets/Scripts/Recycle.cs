2a36bf7ff04518a416f0b4d7fa89092d487859e0
jpg207 <jpg207@gmail.com>
2016-07-24 14:29:09 +1200

    Paralax

---
+++ Assets/Scripts/Recycle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Recycle : MonoBehaviour {
+
+    public Transform StartPoint;
+    public Transform EndPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (transform.position.x < EndPoint.position.x)
+        {
+            float Gap = EndPoint.position.x - transform.position.x;
+            transform.position = new Vector3(StartPoint.position.x - Gap, transform.position.y, transform.position.z);
+        }
+	}
+}
